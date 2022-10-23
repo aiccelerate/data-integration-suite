bb1be3e07a53d3c15d2215afc35d0bb8d0f3622b
A. Anil Sinaci <anil@srdc.com.tr>
2022-10-23 21:46:40 +0300

    :rocket: Improve the docker compose and toFHIR config files.

--- src/test/scala/eu/aiccelerate/dis/Pilot1IntegrationTest.scala
+++ src/test/scala/eu/aiccelerate/dis/Pilot1IntegrationTest.scala
@@ -5,10 +5,10 @@ import akka.http.scaladsl.model.StatusCodes
 import io.onfhir.api.Resource
 import io.onfhir.client.OnFhirNetworkClient
 import io.onfhir.path.FhirPathEvaluator
-import io.onfhir.tofhir.engine._
-import io.onfhir.tofhir.model._
-import io.onfhir.tofhir.util.FhirMappingUtility
 import io.onfhir.util.JsonFormatter._
+import io.tofhir.engine.mapping.{FhirMappingFolderRepository, FhirMappingJobManager, IFhirMappingRepository, IMappingContextLoader, MappingContextLoader, SchemaFolderRepository}
+import io.tofhir.engine.model.{FhirMappingTask, FhirRepositorySinkSettings, FileSystemSource, FileSystemSourceSettings}
+import io.tofhir.engine.util.FhirMappingUtility
 import org.json4s.JArray
 import org.json4s.JsonAST.JObject
 
